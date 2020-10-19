abcd31caf439a13bcae166d16a4145e43687c551
Jung Minwoo <46263190+Jung-Min-woo@users.noreply.github.com>
2020-10-19 19:49:19 +0900

    lvalue_reference++

--- Cpp_MarkGregoire/lvalue_reference.cpp
+++ Cpp_MarkGregoire/lvalue_reference.cpp
@@ -6,9 +6,18 @@ void handleMessage(std::string& message) {
 void handleMessage(std::string&& message) {
 	std::cout << "handleMessage with rvalue reference" << std::endl;
 }
+
 int main(void) {
 	std::string a = "Hello ";
 	std::string b = "world";
 	handleMessage(a);
 	handleMessage(a + b);
+	handleMessage(std::move(a)); // Call handleMessage(string&&) 
+	
+	//rvalue 
+	// int& i = 2; //ERROR : const N Reference
+	// int& j = a+b; //ERROR : TEMPORAY Object Reference
+	int&& i = 2;
+	int c = 2, d = 3;
+	int&& j = c + d;
 }
