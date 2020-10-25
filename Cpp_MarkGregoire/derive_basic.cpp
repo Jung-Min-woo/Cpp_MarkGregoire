3b534ce386d9c4f9e2017a975d5a1416f82bc963
Jung Minwoo <46263190+Jung-Min-woo@users.noreply.github.com>
2020-10-25 14:34:11 +0900

    override 2

--- Cpp_MarkGregoire/derive_basic.cpp
+++ Cpp_MarkGregoire/derive_basic.cpp
@@ -15,7 +15,7 @@ class Derived final : public Base  {
 public :
 	virtual void someOtherMethod();
 	//ex1
-	virtual void someMethod(double d);
+	virtual void someMethod(double d) override// override keyword keeps COMPILE ERR override;
 };
 void Derived::someMethod(double d) {
 	cout << "This is Derived's verion of someMethod()" << endl;
@@ -28,6 +28,6 @@ void Derived::someOtherMethod() {
 int main(void) {
 	Derived myderived;
 	Base& ref = myderived;
-	ref.someMethod(1.1); // Called Derived version
-	
+	ref.someMethod(1.1); // Called Base version
+	ref.someMethod(1); //Called Derived version
 }
