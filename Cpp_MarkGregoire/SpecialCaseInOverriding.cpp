ce2e25e1c4279090e260dbc8c3d97e528b72794b
Jung Minwoo <46263190+Jung-Min-woo@users.noreply.github.com>
2020-11-03 05:54:11 +0900

    private/protected Base class Method

--- Cpp_MarkGregoire/SpecialCaseInOverriding.cpp
+++ Cpp_MarkGregoire/SpecialCaseInOverriding.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class MilesEstimator {
+public:
+	virtual ~MilesEstimator() = default;
+	virtual int getMilesLeft() const { return getMilesPerGallon() * getGallonsLeft(); }
+	virtual void setGallonsLeft(int gallons) { mGallonsLeft = gallons; }
+	virtual int getGallonsLeft() const { return mGallonsLeft; }
+private:
+	int mGallonsLeft;
+	virtual int getMilesPerGallon() const { return 20; }
+};
+class EfficientCarMilesEstimator : public MilesEstimator {
+private:
+	virtual int getMilesPerGallon() const override { return 35; }
+};
 class Base {
 public:
 	virtual ~Base() = default;
@@ -41,7 +55,16 @@ int main(void) {
 	Base& ref2 = myDerived;
 	ref2.overload(); //Call Derived
 	ref2.overload(1); 
-
 	myDerived.overload();
 	myDerived.overload(1); // using Base::overload makes #46 Clear error
+
+	/*ex4*/
+	MilesEstimator myMilesEstimator;
+	myMilesEstimator.setGallonsLeft(2);
+	cout << "Normal estimator can go " << myMilesEstimator.getMilesLeft()
+		<< " more miles" << endl;
+	EfficientCarMilesEstimator myEstimator;
+	myEstimator.setGallonsLeft(2);
+	cout << "Efficient estimator can go " << myEstimator.getMilesLeft()
+		<< " more miles" << endl;
 }
