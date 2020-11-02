e1dcb8f0105b87617095ce22812f0382f5c7c5c4
Jung Minwoo <46263190+Jung-Min-woo@users.noreply.github.com>
2020-11-03 05:42:17 +0900

    overload base class Method

--- Cpp_MarkGregoire/SpecialCaseInOverriding.cpp
+++ Cpp_MarkGregoire/SpecialCaseInOverriding.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class BaseStatic {
+class Base {
 public:
+	virtual ~Base() = default;
+	virtual void overload() { cout << "Base's overload()" << endl; }
+	virtual void overload(int i) { cout << "Base's overload(int i)" << endl; }
+};
+class Derived : public Base {
+public:
+	using Base::overload;
+	virtual void overload() override {
+		cout << "Dervied's overload" << endl;
+	}
+};
+class BaseStatic {
+public:	
 	static void beStatic() {
 		cout << "BaseStatic Being Static" << endl;
 	}
@@ -23,4 +36,12 @@ int main(void) {
 	BaseStatic& ref = myDerviedStatic;
 	myDerviedStatic.beStatic();
 	ref.beStatic();
+	/*ex3*/
+	Derived myDerived;
+	Base& ref2 = myDerived;
+	ref2.overload(); //Call Derived
+	ref2.overload(1); 
+
+	myDerived.overload();
+	myDerived.overload(1); // using Base::overload makes #46 Clear error
 }
