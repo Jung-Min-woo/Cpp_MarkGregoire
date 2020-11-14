77bd53fa3170bfe619ae7720d7b5bbdcfc772021
JungMinwoo <jungmw95@gmail.com>
2020-11-14 16:51:36 +0900

    scope

---
+++ Cpp_MarkGregoire/scope.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<memory>
+class Demo{
+    public:
+        static int get() {return 5;}
+};
+int get() { return 10; }
+namespace NS{
+    int get() {return 20;}
+}
+int main(void){
+    auto pd = std::make_unique<Demo>();
+    Demo d;
+    std::cout<<pd->get() <<std::endl;
+    std::cout<<d.get()<<std::endl;
+    std::cout<<NS::get()<<std::endl;
+    std::cout<<Demo::get()<<std::endl;
+    std::cout<<::get()<<std::endl;
+    std::cout<<get()<<std::endl;
+    return 0;
+}
