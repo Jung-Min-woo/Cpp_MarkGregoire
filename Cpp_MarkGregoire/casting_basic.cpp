fc28d209bf2533c3da3cb848d473ee9efbc0633c
JungMinwoo <jungmw95@gmail.com>
2020-11-14 16:17:42 +0900

    casting_comment applied

--- Cpp_MarkGregoire/casting_basic.cpp
+++ Cpp_MarkGregoire/casting_basic.cpp
@@ -19,6 +19,17 @@ class Derived:public Base{
 class X{};
 class Y{};
 int main(void){
+    //instruction
+    //const 속성 제거 : const_cast()
+    //언어에서 허용하는 명시적 : static_cast()
+    //사용자 정의 생성자나 변환 연산자에서 지원하는 명시적 변환 :static_cast()
+    //서로 관련없는 타입의 객체끼리 변환 : impossible
+    //same 상속 계층에 있는 클래스 타입의 객체 포인터 사이의 변환 : dynamic_cast() 권장, static_cast도 가능
+    //same 상속 계층에 있는 클래스 타입의 객체 레퍼런스 사이의 변환 : dynamic_cast() 권장, static_cast도 가능
+    //서로 관련 없는 타입의 포인터 사이 변환 : reinterpret_cast()
+    //서로 관련 없는 타입의 레퍼런스 사이 변환 : reinterpret_cast()
+    //함수 포인터 사이의 변환 : reinterpret_cast()
+
     //ex1 as_const #include<utility>
     std::string str = "C++";
     const std::string& constStr = std::as_const(str);
