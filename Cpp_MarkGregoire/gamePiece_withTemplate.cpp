227b9c09f5057a9c9830ff3886191084dad98392
JungMinwoo <jungmw95@gmail.com>
2020-11-15 20:31:45 +0900

    gamePiece with Template

--- Cpp_MarkGregoire/gamePiece_withTemplate.cpp
+++ Cpp_MarkGregoire/gamePiece_withTemplate.cpp
@@ -27,4 +27,32 @@ class Grid{
         std::vector<std::vector<std::optional<T>>> mCells;
         size_t mWidth, mHeight;
 };
-int main(void){}
+template<typename T>
+Grid<T>::Grid(size_t width, size_t height) : mWidth(width), mHeight(height){
+    mCells.resize(mWidth);
+    for(auto& column : mCells)
+        column.resize(mHeight);
+}
+template<typename T>
+void Grid<T>::verifyCoordinate(size_t x, size_t y) const{
+    if(x>=mWidth || y>= mHeight)
+        throw std::out_of_range("");
+}
+template<typename T>
+const std::optional<T>& Grid<T>::at(size_t x, size_t y) const{
+    verifyCoordinate(x, y);
+    return mCells[x][y];
+}
+template<typename T>
+std::optional<T>& Grid<T>::at(size_t x, size_t y){
+    return const_cast<std::optional<T>&>(std::as_const(*this).at(x,y));
+}
+int main(void){
+    Grid<int> myIntGrid;
+    Grid<double> mDoubleGrid(11,11);
+    myIntGrid.at(0,0) = 10;
+    int x = myIntGrid.at(0,0).value_or(0);
+    Grid<int> grid2(myIntGrid); // copy ctor
+    Grid<int> anotherIntGrid;
+    anotherIntGrid = grid2;
+}
