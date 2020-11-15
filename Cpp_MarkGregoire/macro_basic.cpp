f6cac9353ce3dfaf38c0d62cff04a235db488a54
JungMinwoo <jungmw95@gmail.com>
2020-11-15 14:45:09 +0900

    Macro_basic(add comments)

--- Cpp_MarkGregoire/macro_basic.cpp
+++ Cpp_MarkGregoire/macro_basic.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #define SQUARE(x) ((x)*(x))
+//use inline function instead use MACRO
+//ex1) cout<<SQUARE(veryExpensiveFunctionCallToComputeNumbr()))
+// --> makes compute twice works
 using namespace std;
 int main(void){
     cout<<SQUARE(5)<<endl;
