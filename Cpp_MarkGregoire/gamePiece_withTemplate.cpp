0c16d487667ad0b0e970a851c694504c8906b109
JungMinwoo <jungmw95@gmail.com>
2020-11-15 15:51:19 +0900

    test

--- Cpp_MarkGregoire/gamePiece_withTemplate.cpp
+++ Cpp_MarkGregoire/gamePiece_withTemplate.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
-using namespace std;
-class TicTacToePiece : public GamePiece{
+#include <optional>
+#include <vector>
+template <typename T>
+class Grid{
     public:
-        virtual std::unique_pt<GamePiece> clone() const override;
-};
-std::unique_ptr<GamePiece> TicTacToePiece::clone()  const{
-    return std::make_unique<TicTacToePiece>(*this);
-}
-int main(void){
+        explicit Grid(size_t width = kDefaultWidth, size_t height = kDefaultHeight);
+        virtual ~ Grid() = default;
 
-}
+    Grid(const Grid& src) = default;
+    Grid<T>& operator=(const Grid& rhs) = default;
+    
+    Grid(Grid&& src) = default;
+    Grid<T>& operator=(Grid&& rhs) = default;
+    
+    std::optional<T>& at(size_t x, size_t y);
+    const std::optional<T>& at(size_t x, size_t y) const;
+
+    size_t getHeight() const { return mHeight;}
+    size_t getWidth() const{ return mWidth;}
+
+    static const size_t kDefaultHeight = 10;
+    static const size_t kDefaultWidth = 10;
+    
+    private:
+        void verifyCoordinate(size_t x, size_t y) const;
+        std::vector<std::vector<std::optional<T>>> mCells;
+        size_t mWidth, mHeight;
+};
+int main(void){}
